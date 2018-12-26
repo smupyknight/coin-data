582fd525b729b4a94e05856fa581cdbcae1a11cd
StarNeit <smupyknight@outlook.com>
2018-12-26 10:17:48 +0100

    Update block files

--- masternode-conf.sh
+++
@@ -1,2 +0,0 @@
-publicIP="$(curl ifconfig.me)"
-echo -e "\nMasternode $publicIP:30808 88LGgy5S3QR6qvRdhu5MkCRC6QjoRvUX14JVCpsa8v29BHYUYwE eb800b51981328b78225bb2c5a6e7f7affe4b855a55ec3e65f5f556e17f00606 1" >> dwe.conf
